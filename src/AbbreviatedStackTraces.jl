9ccdbfe2099e544e593e219eafc6d608ce05eec0
Nicholas Bauer <nicholasbauer@outlook.com>
2021-04-23 20:06:30 -0400

    Revamp, got full behavior working

--- src/AbbreviatedStackTraces.jl
+++ src/AbbreviatedStackTraces.jl
@@ -1,36 +1,123 @@
 module AbbreviatedStackTraces
 
+import REPL:
+    print_response
+
 import Base:
-    StackTraces.top_level_scope_sym,
-    StackFrame,
+    BIG_STACKTRACE_SIZE,
+    catch_stack,
+    invokelatest,
+    printstyled,
+    scrub_repl_backtrace,
+    show,
+    showerror,
     show_backtrace,
-    show_reduced_backtrace,
+    show_exception_stack,
     show_full_backtrace,
-    StackTraces.is_top_level_frame,
+    StackFrame,
     STACKTRACE_FIXEDCOLORS,
     STACKTRACE_MODULECOLORS,
-    BIG_STACKTRACE_SIZE,
     stacktrace_linebreaks,
-    scrub_repl_backtrace,
     print_stackframe,
-    process_backtrace,
-    show,
-    showerror
+    process_backtrace
 
-struct ExceptionInfo
-    error::Any
-    stack::Vector{StackFrame}
+import Base.StackTraces:
+    top_level_scope_sym
+
+function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
+    #= Show the lowest stackframe and display a message telling user how to
+    retrieve the full trace =#
+
+    num_frames = length(trace)
+    ndigits_max = ndigits(num_frames)
+
+    modulecolordict = copy(STACKTRACE_FIXEDCOLORS)
+    modulecolorcycler = Iterators.Stateful(Iterators.cycle(STACKTRACE_MODULECOLORS))
+
+    function print_omitted_modules(i, j)
+        # Find modules involved in intermediate frames and print them
+        modules = filter(!isnothing, unique(t[1] |> parentmodule for t ∈ @view trace[i:j]))
+        length(modules) > 0 || return
+        print(io, repeat(' ', ndigits_max + 2))
+        for m ∈ modules
+            modulecolor = get_modulecolor!(modulecolordict, m, modulecolorcycler)
+            printstyled(io, m, color = modulecolor)
+            print(io, " ")
+        end
+        println(io)
+    end
+
+    # find all frames that aren't in Julia base, stdlib, or an added package
+    is = findall(trace) do frame
+        file = String(frame[1].file)
+        !contains(file, r"[/\\].julia[/\\]packages[/\\]|[/\\]julia[/\\]stdlib") &&
+        (!startswith(file, r".[/\\]") || startswith(file, r".[/\\]REPL")) ||
+        (frame[1].func == top_level_scope_sym)
+    end
+    # include one frame lower
+    is = sort(union(is, is .- 1))
+
+    if length(is) > 0
+        println(io, "\nStacktrace:")
+
+        if is[1] > 0
+            print_omitted_modules(1, is[1])
+            println(io, repeat(' ', ndigits_max + 2) * "⋮")
+        end
+
+        lasti = first(is)
+        @views for i ∈ is
+            if i > 0
+                if i > lasti + 1
+                    println(io, repeat(' ', ndigits_max + 2) * "⋮")
+                    print_omitted_modules(lasti + 1, i - 1)
+                    println(io, repeat(' ', ndigits_max + 2) * "⋮")
+                end
+                print_stackframe(io, i, trace[i][1], trace[i][2], ndigits_max, modulecolordict, modulecolorcycler)
+                if i < num_frames
+                    println(io)
+                    print_linebreaks && println(io)
+                end
+            end
+            lasti = i
+        end
+    end
+
+    length(trace) > length(is) && print(io, "\nUse `err` to retrieve the full stack trace.")
 end
 
-show(io::IO, ex::ExceptionInfo) = display_full_error(io, [(ex.error, ex.stack)])
+function get_modulecolor!(modulecolordict, m, modulecolorcycler)
+    if m !== nothing
+        while parentmodule(m) !== m
+            pm = parentmodule(m)
+            pm == Main && break
+            m = pm
+        end
+        if !haskey(modulecolordict, m)
+            modulecolordict[m] = popfirst!(modulecolorcycler)
+        end
+        return modulecolordict[m]
+    else
+        return :default
+    end
+end
 
-function display_full_error(io::IO, stack::Vector)
+# copied from client.jl with added compacttrace argument
+function display_error(io::IO, er, bt, compacttrace = false)
     printstyled(io, "ERROR: "; bold=true, color=Base.error_color())
-    show_full_exception_stack(IOContext(io, :limit => true), stack)
+    bt = scrub_repl_backtrace(bt)
+    showerror(IOContext(io, :limit => true), er, bt; backtrace = bt!==nothing, compacttrace)
+    println(io)
+end
+function display_error(io::IO, stack::Vector, compacttrace = false)
+    printstyled(io, "ERROR: "; bold=true, color=Base.error_color())
+    bt = Any[ (x[1], scrub_repl_backtrace(x[2])) for x in stack ]
+    show_exception_stack(IOContext(io, :limit => true), bt, compacttrace)
     println(io)
 end
 
-function show_full_exception_stack(io::IO, stack::Vector)
+# copied from errorshow.jl with added compacttrace argument
+function show_exception_stack(io::IO, stack::Vector, compacttrace = false)
     # Display exception stack with the top of the stack first.  This ordering
     # means that the user doesn't have to scroll up in the REPL to discover the
     # root cause.
@@ -40,21 +127,29 @@ function show_full_exception_stack(io::IO, stack::Vector)
             printstyled(io, "\ncaused by: ", color=error_color())
         end
         exc, bt = stack[i]
-        showerror(io, exc, bt; backtrace = bt!==nothing, compacttrace = false)
+        showerror(io, exc, bt; backtrace = bt!==nothing, compacttrace)
         i == 1 || println(io)
     end
 end
-
-function showerror(io::IO, ex, bt; backtrace=true, compacttrace=true)
-    compacttrace && ccall(:jl_set_global, Cvoid, (Any, Any, Any), Main, :err, ExceptionInfo(ex, bt))
+function showerror(io::IO, ex, bt; backtrace=true, compacttrace=false)
     try
         showerror(io, ex)
     finally
         backtrace && show_backtrace(io, bt, compacttrace)
     end
 end
-
-function Base.show_backtrace(io::IO, t::Vector, compacttrace = true)
+function showerror(io::IO, ex::LoadError, bt; backtrace=true, compacttrace=false)
+    !isa(ex.error, LoadError) && print(io, "LoadError: ")
+    showerror(io, ex.error, bt; backtrace=backtrace, compacttrace)
+    print(io, "\nin expression starting at $(ex.file):$(ex.line)")
+end
+showerror(io::IO, ex::LoadError) = showerror(io, ex, [])
+function showerror(io::IO, ex::InitError, bt; backtrace=true, compacttrace=false)
+    print(io, "InitError: ")
+    showerror(io, ex.error, bt; backtrace=backtrace, compacttrace)
+    print(io, "\nduring initialization of module ", ex.mod)
+end
+function show_backtrace(io::IO, t::Vector, compacttrace = false)
     if haskey(io, :last_shown_line_infos)
         empty!(io[:last_shown_line_infos])
     end
@@ -90,81 +185,69 @@ function Base.show_backtrace(io::IO, t::Vector, compacttrace = true)
     return
 end
 
-function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
-    #= Show the lowest stackframe and display a message telling user how to
-    retrieve the full trace =#
-
-    num_frames = length(trace)
-    ndigits_max = ndigits(num_frames)
-
-    modulecolordict = copy(STACKTRACE_FIXEDCOLORS)
-    modulecolorcycler = Iterators.Stateful(Iterators.cycle(STACKTRACE_MODULECOLORS))
-
-    function print_omitted_modules(i, j)
-        # Find modules involved in intermediate frames and print them
-        modules = filter(!isnothing, unique(t[1] |> parentmodule for t ∈ @view trace[i:j]))
-        length(modules) > 0 || return
-        print(io, repeat(' ', ndigits_max + 2))
-        for m ∈ modules
-            modulecolor = get_modulecolor!(modulecolordict, m, modulecolorcycler)
-            printstyled(io, m, color = modulecolor)
-            print(io, " ")
-        end
-        println(io)
+#copied from task.jl with added compacttrace argument
+function showerror(io::IO, ex::TaskFailedException, bt = nothing; backtrace=true, compacttrace=false)
+    print(io, "TaskFailedException")
+    if bt !== nothing && backtrace
+        show_backtrace(io, bt)
     end
-
-    # find all frames that aren't in Julia base, stdlib, or an added package
-    is = findall(trace) do frame
-        file = String(frame[1].file)
-        !contains(file, r"[/\\].julia[/\\]packages[/\\]|[/\\]julia[/\\]stdlib") &&
-        (!startswith(file, r".[/\\]") || startswith(file, r".[/\\]REPL")) ||
-        (frame[1].func == top_level_scope_sym)
-    end
-
-    # include one frame lower
-    is = filter(>(0), sort(union(is, is .- 1)))
-
-    if length(is) > 0
-        println(io, "\nStacktrace:")
-
-        if is[1] > 1
-            print_omitted_modules(1, is[1])
-            println(io, repeat(' ', ndigits_max + 2) * "⋮")
-        end
-
-        lasti = first(is)
-        @views for i ∈ is
-            if i > lasti + 1
-                println(io, repeat(' ', ndigits_max + 2) * "⋮")
-                print_omitted_modules(lasti + 1, i - 1)
-                println(io, repeat(' ', ndigits_max + 2) * "⋮")
-            end
-            print_stackframe(io, i, trace[i][1], trace[i][2], ndigits_max, modulecolordict, modulecolorcycler)
-            if i < num_frames
-                println(io)
-                print_linebreaks && println(io)
-            end
-            lasti = i
-        end
-    end
-
-    length(trace) > length(is) && print(io, "\nUse `err` to retrieve the full stack trace.")
+    println(io)
+    printstyled(io, "\n    nested task error: ", color=error_color())
+    show_task_exception(io, ex.task)
 end
 
-function get_modulecolor!(modulecolordict, m, modulecolorcycler)
-    if m !== nothing
-        while parentmodule(m) !== m
-            pm = parentmodule(m)
-            pm == Main && break
-            m = pm
+struct ExceptionInfo
+    errors::Vector{Tuple{Any, Vector{Union{Ptr{Nothing}, Base.InterpreterIP}}}}
+end
+
+show(io::IO, exs::ExceptionInfo) = display_error(io, exs.errors)
+
+# copied from REPL.jl with addition of :err global
+function print_response(errio::IO, response, show_value::Bool, have_color::Bool, specialdisplay::Union{AbstractDisplay,Nothing}=nothing)
+    Base.sigatomic_begin()
+    val, iserr = response
+    while true
+        try
+            Base.sigatomic_end()
+            if iserr
+                ccall(:jl_set_global, Cvoid, (Any, Any, Any), Main, :err, ExceptionInfo(val))
+                Base.invokelatest(display_error, errio, val, true)
+            else
+                if val !== nothing && show_value
+                    try
+                        if specialdisplay === nothing
+                            Base.invokelatest(display, val)
+                        else
+                            Base.invokelatest(display, specialdisplay, val)
+                        end
+                    catch
+                        println(errio, "Error showing value of type ", typeof(val), ":")
+                        rethrow()
+                    end
+                end
+            end
+            break
+        catch
+            if iserr
+                println(errio) # an error during printing is likely to leave us mid-line
+                println(errio, "SYSTEM (REPL): showing an error caused an error")
+                try
+                    Base.invokelatest(Base.display_error, errio, catch_stack())
+                catch e
+                    # at this point, only print the name of the type as a Symbol to
+                    # minimize the possibility of further errors.
+                    println(errio)
+                    println(errio, "SYSTEM (REPL): caught exception of type ", typeof(e).name.name,
+                            " while trying to handle a nested exception; giving up")
+                end
+                break
+            end
+            val = catch_stack()
+            iserr = true
         end
-        if !haskey(modulecolordict, m)
-            modulecolordict[m] = popfirst!(modulecolorcycler)
-        end
-        return modulecolordict[m]
-    else
-        return :default
     end
+    Base.sigatomic_end()
+    nothing
 end
 
 end
