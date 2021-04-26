2ec5d2eb6a4bbd7649cabbcdcf0edd0d867ae9b6
Nicholas Bauer <nicholasbauer@outlook.com>
2021-04-25 21:07:35 -0400

    Changed display to have two lines and frame range, add int frames back

--- src/AbbreviatedStackTraces.jl
+++ src/AbbreviatedStackTraces.jl
@@ -46,7 +46,7 @@ function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
     retrieve the full trace =#
 
     num_frames = length(trace)
-    ndigits_max = ndigits(num_frames)
+    ndigits_max = ndigits(num_frames) * 2 + 1
 
     modulecolordict = copy(STACKTRACE_FIXEDCOLORS)
     modulecolorcycler = Iterators.Stateful(Iterators.cycle(STACKTRACE_MODULECOLORS))
@@ -55,7 +55,11 @@ function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
         # Find modules involved in intermediate frames and print them
         modules = filter(!isnothing, unique(t[1] |> parentmodule for t ∈ @view trace[i:j]))
         length(modules) > 0 || return
+        print(io, " " ^ (ndigits_max - ndigits(i) - ndigits(j)))
+        print(io, "[" * string(i) * "-" * string(j) * "] ")
+        println(io, "⋮")
         print(io, " " ^ (ndigits_max + 2))
+        printstyled(io, "@ ", color = :light_black)
         for m ∈ modules
             modulecolor = get_modulecolor!(modulecolordict, m, modulecolorcycler)
             printstyled(io, m, color = modulecolor)
@@ -85,30 +89,23 @@ function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
     end, is)
     setdiff!(internali, internali .+ 1)
 
-    # include the highest immediate hidden frame called into from user-controlled code
-    filter!(>(0), sort!(union!(is, minimum(union!(is .- 1, internali .- 1)))))
-
-    # add back top-level if not already present; and if it is, add the next one
-    if length(trace) ∈ is
-        sort!(union!(is, length(trace) - 1))
-    else
-        push!(is, length(trace))
-    end
+    # include the next immediate hidden frame called into from user-controlled code
+    filter!(>(0), sort!(union!(is, union!(is .- 1, internali .- 1))))
 
     if length(is) > 0
+        # remove top-level if present
+        is[end] == length(trace) && pop!(is)
+
         println(io, "\nStacktrace:")
 
         if is[1] > 1
             print_omitted_modules(1, is[1] - 1)
-            println(io, repeat(' ', ndigits_max + 2) * "⋮")
         end
 
         lasti = first(is)
         @views for i ∈ is
             if i > lasti + 1
-                println(io, repeat(' ', ndigits_max + 2) * "⋮")
                 print_omitted_modules(lasti + 1, i - 1)
-                println(io, repeat(' ', ndigits_max + 2) * "⋮")
             end
             print_stackframe(io, i, trace[i][1], trace[i][2], ndigits_max, modulecolordict, modulecolorcycler)
             if i < num_frames
@@ -117,9 +114,11 @@ function show_compact_backtrace(io::IO, trace::Vector; print_linebreaks::Bool)
             end
             lasti = i
         end
+    else
+        println(io)
     end
 
-    length(trace) > length(is) && print(io, "\nUse `err` to retrieve the full stack trace.")
+    length(trace) > length(is) && print(io, "Use `err` to retrieve the full stack trace.")
 end
 
 function get_modulecolor!(modulecolordict, m, modulecolorcycler)
