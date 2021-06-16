47e57b51c5692efd879a809bf287c7456a58dff1
Nicholas Bauer <nicholasbauer@outlook.com>
2021-06-16 14:59:45 -0400

    Elide type parameters if there are more than 2

--- src/AbbreviatedStackTraces.jl
+++ src/AbbreviatedStackTraces.jl
@@ -4,6 +4,9 @@ __precompile__(false)
 import REPL:
     print_response
 
+import Core:
+    SimpleVector
+
 import Base:
     BIG_STACKTRACE_SIZE,
     CodeInfo,
@@ -20,9 +23,11 @@ import Base:
     show,
     showerror,
     show_backtrace,
+    show_datatype,
     show_exception_stack,
     show_full_backtrace,
     show_tuple_as_call,
+    show_type_name,
     StackFrame,
     stacktrace_contract_userdir,
     stacktrace_expand_basepaths,
@@ -465,7 +470,7 @@ function show_spec_linfo(io::IO, frame::StackFrame)
                                             kwargs=zip(kwnames, kwarg_types),
                                             argnames=argnames[def.nkw+2:end])
                 else
-                    Base.show_tuple_as_call(io, def.name, sig; demangle=true, argnames)
+                    Base.invokelatest(show_tuple_as_call, io, def.name, sig; demangle=true, argnames)
                 end
             end
         else
@@ -473,6 +478,35 @@ function show_spec_linfo(io::IO, frame::StackFrame)
         end
     elseif linfo isa CodeInfo
         print(io, "top-level scope")
+    end
+end
+
+function show_datatype(io::IO, @nospecialize(x::DataType))
+    parameters = x.parameters::SimpleVector
+    istuple = x.name === Tuple.name
+    n = length(parameters)
+
+    # Print homogeneous tuples with more than 3 elements compactly as NTuple{N, T}
+    if istuple && n > 3 && all(i -> (parameters[1] === i), parameters)
+        print(io, "NTuple{", n, ", ", parameters[1], "}")
+    else
+        show_type_name(io, x.name)
+        if (n > 0 || istuple) && x !== Tuple
+            # Do not print the type parameters for the primary type if we are
+            # printing a method signature or type parameter.
+            # Always print the type parameter if we are printing the type directly
+            # since this information is still useful.
+            print(io, '{')
+            omitted = get(io, :compacttrace, false) && n > 2
+            omitted && (n = 2)
+            for i = 1:n
+                p = parameters[i]
+                show(io, p)
+                i < n && print(io, ", ")
+            end
+            omitted && print(io, ", …")
+            print(io, '}')
+        end
     end
 end
 
