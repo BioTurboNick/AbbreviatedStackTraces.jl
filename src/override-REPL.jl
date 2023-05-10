91b94b3c6283225cb4cf6341e995e1ab56c83982
Nicholas Bauer <nicholasbauer@outlook.com>
2023-05-09 23:09:47 -0400

    Add v1.9 version of `print_response`

--- src/override-REPL.jl
+++ src/override-REPL.jl
@@ -50,7 +50,7 @@ if VERSION < v"1.8.0-DEV.1040"
         nothing
     end
 
-else
+elseif VERSION < v"1.9"
 
     function print_response(errio::IO, response, show_value::Bool, have_color::Bool, specialdisplay::Union{AbstractDisplay,Nothing}=nothing)
         Base.sigatomic_begin()
@@ -102,4 +102,55 @@ else
         nothing
     end
     
+else
+    
+    function print_response(errio::IO, response, show_value::Bool, have_color::Bool, specialdisplay::Union{AbstractDisplay,Nothing}=nothing)
+        Base.sigatomic_begin()
+        val, iserr = response
+        while true
+            try
+                Base.sigatomic_end()
+                if iserr
+                    val = Base.scrub_repl_backtrace(val)
+                    Base.istrivialerror(val) || setglobal!(Main, :err, val)
+                    Base.invokelatest(Base.display_error, errio, val, true)
+                else
+                    if val !== nothing && show_value
+                        try
+                            if specialdisplay === nothing
+                                Base.invokelatest(display, val)
+                            else
+                                Base.invokelatest(display, specialdisplay, val)
+                            end
+                        catch
+                            println(errio, "Error showing value of type ", typeof(val), ":")
+                            rethrow()
+                        end
+                    end
+                end
+                break
+            catch ex
+                if iserr
+                    println(errio) # an error during printing is likely to leave us mid-line
+                    println(errio, "SYSTEM (REPL): showing an error caused an error")
+                    try
+                        excs = Base.scrub_repl_backtrace(current_exceptions())
+                        setglobal!(Main, :err, excs)
+                        Base.invokelatest(Base.display_error, errio, excs)
+                    catch e
+                        # at this point, only print the name of the type as a Symbol to
+                        # minimize the possibility of further errors.
+                        println(errio)
+                        println(errio, "SYSTEM (REPL): caught exception of type ", typeof(e).name.name,
+                                " while trying to handle a nested exception; giving up")
+                    end
+                    break
+                end
+                val = current_exceptions()
+                iserr = true
+            end
+        end
+        Base.sigatomic_end()
+        nothing
+    end
 end
