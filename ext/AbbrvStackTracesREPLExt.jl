ad3fbd0525e6ce4f9768a711e9180831b455c030
Nicholas Bauer <nicholasbauer@outlook.com>
2025-12-02 16:00:55 -0500

    Remove no-longer-needed overrides that were causing world age issues

--- ext/AbbrvStackTracesREPLExt.jl
+++ ext/AbbrvStackTracesREPLExt.jl
@@ -9,69 +9,6 @@ import Base:
 
 if VERSION ≥ v"1.11"
     REPL.repl_display_error(errio::IO, @nospecialize errval) = repl_display_error_abbrv(errio, errval)
-end
-
-
-
-if VERSION ≥ v"1.12-alpha"
-    function REPL.print_response(errio::IO, response, backend::Union{REPL.REPLBackendRef,Nothing}, show_value::Bool, have_color::Bool, specialdisplay::Union{AbstractDisplay,Nothing}=nothing)
-        Base.sigatomic_begin()
-        val, iserr = response
-        while true
-            try
-                Base.sigatomic_end()
-                if iserr
-                    val = Base.scrub_repl_backtrace(val)
-                    Base.istrivialerror(val) || setglobal!(Base.MainInclude, :err, val)
-                    REPL.repl_display_error(errio, val)
-                else
-                    if val !== nothing && show_value
-                        val2, iserr = if specialdisplay === nothing
-                            # display calls may require being run on the main thread
-                            REPL.call_on_backend(backend) do
-                                Base.invokelatest(display, val)
-                            end
-                        else
-                            REPL.call_on_backend(backend) do
-                                Base.invokelatest(display, specialdisplay, val)
-                            end
-                        end
-                        if iserr
-                            println(errio, "Error showing value of type ", typeof(val), ":")
-                            throw(val2)
-                        end
-                    end
-                end
-                break
-            catch ex
-                if iserr
-                    println(errio) # an error during printing is likely to leave us mid-line
-                    println(errio, "SYSTEM (REPL): showing an error caused an error")
-                    try
-                        excs = Base.scrub_repl_backtrace(current_exceptions())
-                        setglobal!(Base.MainInclude, :err, excs)
-                        REPL.repl_display_error(errio, excs)
-                    catch e
-                        # at this point, only print the name of the type as a Symbol to
-                        # minimize the possibility of further errors.
-                        println(errio)
-                        println(errio, "SYSTEM (REPL): caught exception of type ", typeof(e).name.name,
-                                " while trying to handle a nested exception; giving up")
-                    end
-                    break
-                end
-                val = current_exceptions()
-                iserr = true
-            end
-        end
-        Base.sigatomic_end()
-        nothing
-    end
-    function REPL.call_on_backend(f, backend::REPL.REPLBackendRef)
-        applicable(f) || error("internal error: f is not callable")
-        put!(backend.repl_channel, (f, 2)) # (f, show_value) 2 indicates function (rather than ast)
-        return take!(backend.response_channel) # (val, iserr)
-    end
 else
     function REPL.print_response(errio::IO, response, show_value::Bool, have_color::Bool, specialdisplay::Union{AbstractDisplay,Nothing}=nothing)
         Base.sigatomic_begin()
